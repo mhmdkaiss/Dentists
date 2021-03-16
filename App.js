a6c8440781e5344ce460fe5c706475639ba4ce11
mhmd <mohamad_kaiss@hotmail.com>
2021-03-16 12:44:43 +0100

    added bottom icons

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RegistrationForm from './pages/HomeScreen';
 import Feather from 'react-native-vector-icons/Feather';
+import FontAwsome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 // function HomeScreen() {
 //   return (
@@ -35,7 +37,30 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
+      <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+
+                  if (route.name === 'Home') {
+                    iconName = focused ? 'search' : 'search';
+                    return <Feather name={iconName} size={size} color={color} />;
+                  } else if (route.name === 'Settings') {
+                    iconName = focused ? 'heartbeat' : 'heartbeat';
+                    return <FontAwsome name={iconName} size={size} color={color} />;
+                  }else if (route.name === 'Locations') {
+                    iconName = focused ? 'location-pin' : 'location-pin';
+                    return <Entypo name={iconName} size={32} color={color} />;
+                  }
+
+                  // You can return any component that you like here!
+              
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'black',
+                inactiveTintColor: 'gray',
+              }} 
              >
         <Tab.Screen name="Home" component={RegistrationForm} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
