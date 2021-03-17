8874da064ba5782adfd50b5ca0cca0a0a84f9862
mhmd <mohamad_kaiss@hotmail.com>
2021-03-17 18:58:14 +0100

    loctionpage

--- pages/HeartPage.js
+++ pages/HeartPage.js
@@ -1,13 +1,30 @@
-import React,{Component} from 'react';
-import {View,Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {View,Text, StyleSheet,Image, TouchableOpacity} from 'react-native';
+import Card from '../components/Card';
+import CardSection from '../components/CardSection';
+import Button from '../components/Button';
+import Input from '../components/Input'
+import Spinner from '../components/Spinner';
 
-class secondTab extends Component {
-  
+class secondTab extends React.Component {  
+ 
+
   render(){
       return (
-        <View>
-            <Text>2nd tab page</Text>
+        
+        <View style={styles.containerForm}>
+
+        <View style={styles.imageContainer}>
+            <Image style={styles.imageStyle} source={require('../assets/Nord-Quest.png')}/>
         </View>
+        
+        <View style={styles.PublicitesStyleContainer}>
+        <Text style={styles.titleStyle}>Publicites</Text>
+        </View>
+        
+        
+          </View>
+        
 
         
     );
@@ -15,7 +32,30 @@ class secondTab extends Component {
 }
 
 const styles= StyleSheet.create({
- 
+  containerForm:{
+    flex:1,
+    backgroundColor:'white'
+  }
+  ,
+  imageStyle:{
+    alignSelf:'flex-end',
+    height:150,
+    width:100,
+  }
+  ,
+  titleStyle:{
+    paddingRight:100,
+    paddingBottom:100,
+    fontSize:22,
+    color:'grey',
+  }
+  ,
+  PublicitesStyleContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  }
+
 })
 
 
